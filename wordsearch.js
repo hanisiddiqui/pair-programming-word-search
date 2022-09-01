b7839e3ed5fecd61997606b238bb838d5824d073
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-01 03:37:29 +0000

    fixed wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  if (word.length < 2) {
+    return false;
+  }
+
+  const verticalJoin = transpose(letters).map((x) => x.join(""));
+
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+const transpose = (letters) => {
+  const newArray = [];
+
+  for (let i = 0; i < letters.length - 1; i++) {
+    newArray[i] = [];
+
+    for (let j = 0; j < letters[i].length; j++) {
+      newArray[i][j] = letters[j][i];
     }
-}
+  }
+  return newArray;
+};
+
 
 module.exports = wordSearch
